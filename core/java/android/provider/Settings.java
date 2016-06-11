d4760cbfade8959dbadab30b75982564317626da
aow1980 <wilkes.allen@gmail.com>
2016-06-10 22:17:30 -0400

    navigation bar: custom height
    Following this commit: https://github.com/krexus/android_frameworks_base/commit/85ce62a941486d27e30d3727a63817de783ef241

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3450,6 +3450,12 @@ public final class Settings {
             }
         };
 
+         /**
+         * Navigation bar height when it is on protrait
+         * @hide
+         */
+        public static final String NAVIGATION_BAR_HEIGHT = "navigation_bar_height";
+
         /**
          * Beginning of PureNexus System Settings Additions
          * @hide
